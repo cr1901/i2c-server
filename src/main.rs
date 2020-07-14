44e36052bef01ebeb2e3b0968bb5d35d718674b5
William D. Jones <thor0505@comcast.net>
2020-07-14 01:13:08 -0400

    Set temp reading to 12 bits in init. Add CSV format output.

--- src/main.rs
+++ src/main.rs
@@ -12,13 +12,15 @@ const TEMP_SENSOR_ADDR: u16 = 0x48;
 fn i2cfun() -> Result<(), LinuxI2CError> {
     let mut dev = LinuxI2CDevice::new("/dev/i2c-1", TEMP_SENSOR_ADDR)?;
 
+    dev.smbus_write_byte_data(0x01, 0x60)?;
+
     loop {
-        let raw = i16::from_be(dev.smbus_read_word_data(0x00).unwrap() as i16);
-        let cels : f32 = f32::from(raw >> 4) / 16.0;
+        let raw = i16::from_be(dev.smbus_read_word_data(0x00).unwrap() as i16) >> 4;
+        let cels : f32 = f32::from(raw) / 16.0;
         let fahr = 1.8*cels + 32.0;
         
         let now = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_secs();
-        println!("{:?}: {:?}", now, fahr);
+        println!("{:?}, {:?}, {:?}, {:?}", now, raw, cels, fahr);
         thread::sleep(Duration::from_millis(1000));
     }
 }
