dd6cb1c864f8d4e90898d7a8ff1b2c626ed1a68b
William D. Jones <thor0505@comcast.net>
2020-07-15 02:08:26 -0400

    Convert into workspace, make sure code compiles on Windows.

--- server/src/main.rs
+++ server/src/main.rs
@@ -4,11 +4,13 @@ use std::thread;
 use std::time::{SystemTime, Duration};
 
 use i2cdev::core::*;
+#[cfg(unix)]
 use i2cdev::linux::{LinuxI2CDevice, LinuxI2CError};
 
 const TEMP_SENSOR_ADDR: u16 = 0x48;
 
 // real code should probably not use unwrap()
+#[cfg(unix)]
 fn i2cfun() -> Result<(), LinuxI2CError> {
     let mut dev = LinuxI2CDevice::new("/dev/i2c-1", TEMP_SENSOR_ADDR)?;
 
@@ -18,7 +20,7 @@ fn i2cfun() -> Result<(), LinuxI2CError> {
         let raw = i16::from_be(dev.smbus_read_word_data(0x00).unwrap() as i16) >> 4;
         let cels : f32 = f32::from(raw) / 16.0;
         let fahr = 1.8*cels + 32.0;
-        
+
         let now = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_secs();
         println!("{:?}, {:?}, {:?}, {:?}", now, raw, cels, fahr);
         thread::sleep(Duration::from_millis(1000));
@@ -26,5 +28,6 @@ fn i2cfun() -> Result<(), LinuxI2CError> {
 }
 
 fn main() {
-  i2cfun().unwrap();
+    #[cfg(unix)]
+    i2cfun().unwrap();
 }
