0e98200203a50b03475ef1e65522ab15e39da735
William D. Jones <thor0505@comcast.net>
2020-07-15 04:30:10 -0400

    Create skeleton of http server and i2c read loop.

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,13 +1,55 @@
 extern crate i2cdev;
 
+use futures::stream::StreamExt;
 use std::thread;
-use std::time::{SystemTime, Duration};
+use std::time::{Duration, SystemTime};
+use tokio::net::TcpListener;
+use tokio::prelude::*;
+use tokio::time::delay_for;
 
 use i2cdev::core::*;
 #[cfg(unix)]
 use i2cdev::linux::{LinuxI2CDevice, LinuxI2CError};
 
 const TEMP_SENSOR_ADDR: u16 = 0x48;
+
+async fn http_server() -> Result<(), Box<dyn std::error::Error>> {
+    let mut listener = TcpListener::bind("127.0.0.1:8000").await?;
+
+    let server = async move {
+        let mut incoming = listener.incoming();
+
+        while let Some(socket_res) = incoming.next().await {
+            match socket_res {
+                Ok(mut socket) => {
+                    println!("Accepted connection from {:?}", socket.peer_addr());
+
+                    tokio::spawn(async move {
+                        // Split up the reading and writing parts of the
+                        // socket.
+                        let (mut reader, mut writer) = socket.split();
+
+                        match tokio::io::copy(&mut reader, &mut writer).await {
+                            Ok(amt) => {
+                                println!("wrote {} bytes", amt);
+                            }
+                            Err(err) => {
+                                eprintln!("IO error {:?}", err);
+                            }
+                        }
+                    });
+                }
+                Err(err) => {
+                    // Handle error by printing to STDOUT.
+                    println!("accept error = {:?}", err);
+                }
+            }
+        }
+    };
+
+    server.await;
+    Ok(())
+}
 
 // real code should probably not use unwrap()
 #[cfg(unix)]
@@ -18,16 +60,28 @@ fn i2cfun() -> Result<(), LinuxI2CError> {
 
     loop {
         let raw = i16::from_be(dev.smbus_read_word_data(0x00).unwrap() as i16) >> 4;
-        let cels : f32 = f32::from(raw) / 16.0;
-        let fahr = 1.8*cels + 32.0;
+        let cels: f32 = f32::from(raw) / 16.0;
+        let fahr = 1.8 * cels + 32.0;
 
-        let now = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_secs();
+        let now = SystemTime::now()
+            .duration_since(SystemTime::UNIX_EPOCH)
+            .unwrap()
+            .as_secs();
         println!("{:?}, {:?}, {:?}, {:?}", now, raw, cels, fahr);
         thread::sleep(Duration::from_millis(1000));
     }
 }
 
-fn main() {
-    #[cfg(unix)]
-    i2cfun().unwrap();
+#[cfg(windows)]
+async fn i2cfun() -> Result<(), ()> {
+    loop {
+        println!("i2c task");
+        delay_for(Duration::from_millis(1000)).await;
+    }
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() {
+    let (_, _) = tokio::join!(i2cfun(), http_server());
 }
