d24f3d68ab04560fb90e56488f24e3d367c7fb05
William D. Jones <thor0505@comcast.net>
2020-07-18 11:04:31 -0400

    Make sure server runs on Linux.

--- server/src/main.rs
+++ server/src/main.rs
@@ -13,36 +13,47 @@ use std::sync::Arc;
 use std::str;
 
 use base64::{encode_config_slice, URL_SAFE};
-use hyper::{Body, Request, Response, Server};
+use hyper::{Body, Method, Request, Response, StatusCode, Server};
 use hyper::service::{make_service_fn, service_fn};
 
+use i2cdev::core::*;
 #[cfg(unix)]
 use i2cdev::linux::{LinuxI2CDevice, LinuxI2CError};
 #[cfg(unix)]
 const TEMP_SENSOR_ADDR: u16 = 0x48;
 
 
-async fn temp_service(_: Request<Body>, rx: Arc<Mutex<Vec<i16>>>) -> Result<Response<Body>, Infallible> {
-    let max_base64_size = rx.lock().await.capacity() * 4 / 3 + 4;
-    let mut payload = Vec::<u8>::with_capacity(max_base64_size);
+async fn temp_service(req: Request<Body>, rx: Arc<Mutex<Vec<i16>>>) -> Result<Response<Body>, Infallible> {
+    match (req.method(), req.uri().path()) {
+        (&Method::GET, "/") => {
+            let max_base64_size = rx.lock().await.capacity() * 4 / 3 + 4;
+            let mut payload = Vec::<u8>::with_capacity(max_base64_size);
 
-    payload.resize(max_base64_size, 0);
+            payload.resize(max_base64_size, 0);
 
-    let written = {
-        let temp_lock = rx.lock().await;
+            let written = {
+                let temp_lock = rx.lock().await;
 
-        let temp_data = {
-            let temp_ptr = &**temp_lock as *const [i16] as *const i16 as *const u8;
-            let temp_len = temp_lock.len();
-            unsafe { std::slice::from_raw_parts(temp_ptr, temp_len * 2) }
-        };
+                let temp_data = {
+                    let temp_ptr = &**temp_lock as *const [i16] as *const i16 as *const u8;
+                    let temp_len = temp_lock.len();
+                    unsafe { std::slice::from_raw_parts(temp_ptr, temp_len * 2) }
+                };
 
-        encode_config_slice(temp_data, URL_SAFE, &mut payload)
-    };
+                encode_config_slice(temp_data, URL_SAFE, &mut payload)
+            };
 
-    payload.resize(written, 0);
+            payload.resize(written, 0);
 
-    Result::<Response<Body>, Infallible>::Ok(Response::new(Body::from(payload)))
+            Ok(Response::new(Body::from(payload)))
+        },
+
+        _ => {
+            let mut not_found = Response::default();
+            *not_found.status_mut() = StatusCode::NOT_FOUND;
+            Ok(not_found)
+        }
+    }
 }
 
 // real code should probably not use unwrap()
@@ -53,25 +64,12 @@ async fn i2cfun(tx: Arc<Mutex<Vec<i16>>>) -> Result<(), ()> {
     dev.smbus_write_byte_data(0x01, 0x60).unwrap();
 
     loop {
-        let before = SystemTime::now();
-
         // Measured: takes approx 1 millisecond.
         let raw = i16::from_be(dev.smbus_read_word_data(0x00).unwrap() as i16) >> 4;
-        let cels: f32 = f32::from(raw) / 16.0;
-        let fahr = 1.8 * cels + 32.0;
-
-        let now = SystemTime::now();
-        let nix_ts = now.duration_since(SystemTime::UNIX_EPOCH)
-            .unwrap()
-            .as_secs();
-
-        let elapsed = now.duration_since(before).unwrap().as_micros();
 
         let mut lock = tx.lock().await;
-        lock[0] = raw;
+        lock.push(raw);
 
-        //println!("{:?}, {:?}, {:?}, {:?}", now, raw, cels, fahr);
-        println!("i2c task: {:?}", elapsed);
         delay_for(Duration::from_millis(1000)).await;
     }
 }
@@ -85,7 +83,6 @@ async fn i2cfun(tx: Arc<Mutex<Vec<i16>>>) -> Result<(), ()> {
         let mut lock = tx.lock().await;
         lock.push(fake_temp);
 
-        //println!("i2c task: {:?}", elapsed);
         delay_for(Duration::from_millis(1000)).await;
         fake_temp += 1;
     }
@@ -109,7 +106,7 @@ async fn main() {
        }
     });
 
-    let addr = ([127, 0, 0, 1], 8000).into();
+    let addr = ([0, 0, 0, 0], 8000).into();
     let server = Server::bind(&addr).serve(make_svc);
 
     let temp_read = i2cfun(i2c_tx);
