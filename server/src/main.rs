565def0bfdbd557d2ae1ce2d51c54d5b5aedd58f
William D. Jones <thor0505@comcast.net>
2020-07-18 10:36:57 -0400

    Use hyper crate to implement http server.

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,5 +1,7 @@
 extern crate i2cdev;
 
+use std::convert::Infallible;
+
 use futures::stream::StreamExt;
 use std::thread;
 use std::time::{Duration, SystemTime};
@@ -11,57 +13,36 @@ use std::sync::Arc;
 use std::str;
 
 use base64::{encode_config_slice, URL_SAFE};
+use hyper::{Body, Request, Response, Server};
+use hyper::service::{make_service_fn, service_fn};
 
 #[cfg(unix)]
 use i2cdev::linux::{LinuxI2CDevice, LinuxI2CError};
 #[cfg(unix)]
 const TEMP_SENSOR_ADDR: u16 = 0x48;
 
-async fn http_server(rx: Arc<Mutex<Vec<i16>>>) -> Result<(), Box<dyn std::error::Error>> {
-    let mut listener = TcpListener::bind("127.0.0.1:8000").await?;
+
+async fn temp_service(_: Request<Body>, rx: Arc<Mutex<Vec<i16>>>) -> Result<Response<Body>, Infallible> {
     let max_base64_size = rx.lock().await.capacity() * 4 / 3 + 4;
+    let mut payload = Vec::<u8>::with_capacity(max_base64_size);
 
-    let server = async move {
-        let mut incoming = listener.incoming();
+    payload.resize(max_base64_size, 0);
 
-        while let Some(socket_res) = incoming.next().await {
-            match socket_res {
-                Ok(mut socket) => {
-                    println!("Accepted connection from {:?}", socket.peer_addr());
+    let written = {
+        let temp_lock = rx.lock().await;
 
-                    let new_rx = rx.clone();
+        let temp_data = {
+            let temp_ptr = &**temp_lock as *const [i16] as *const i16 as *const u8;
+            let temp_len = temp_lock.len();
+            unsafe { std::slice::from_raw_parts(temp_ptr, temp_len * 2) }
+        };
 
-                    tokio::spawn(async move {
-                        let mut payload = Vec::<u8>::with_capacity(max_base64_size);
-                        payload.resize(max_base64_size, 0);
-
-                        let written = {
-                            let temp_lock = new_rx.lock().await;
-
-                            let temp_data = {
-                                let temp_ptr = &**temp_lock as *const [i16] as *const i16 as *const u8;
-                                let temp_len = temp_lock.len();
-                                unsafe { std::slice::from_raw_parts(temp_ptr, temp_len * 2) }
-                            };
-
-                            encode_config_slice(temp_data, URL_SAFE, &mut payload)
-                        };
-
-                        payload.resize(written, 0);
-                        println!("{}", str::from_utf8(&payload).unwrap());
-                        socket.write_all(&payload).await.unwrap();
-                    });
-                }
-                Err(err) => {
-                    // Handle error by printing to STDOUT.
-                    println!("accept error = {:?}", err);
-                }
-            }
-        }
+        encode_config_slice(temp_data, URL_SAFE, &mut payload)
     };
 
-    server.await;
-    Ok(())
+    payload.resize(written, 0);
+
+    Result::<Response<Body>, Infallible>::Ok(Response::new(Body::from(payload)))
 }
 
 // real code should probably not use unwrap()
@@ -112,10 +93,26 @@ async fn i2cfun(tx: Arc<Mutex<Vec<i16>>>) -> Result<(), ()> {
     Ok(())
 }
 
+
 #[tokio::main]
 async fn main() {
     let i2c_tx = Arc::new(Mutex::new(Vec::<i16>::with_capacity(1024)));
     let i2c_rx  = Arc::clone(&i2c_tx);
 
-    let (_, _) = tokio::join!(i2cfun(i2c_tx), http_server(i2c_rx));
+    let make_svc = make_service_fn(|_conn| {
+       let foo = Arc::clone(&i2c_rx);
+
+       async {
+           Ok::<_, Infallible>(service_fn(move |body: Request<Body>| {
+               temp_service(body, Arc::clone(&foo))
+           }))
+       }
+    });
+
+    let addr = ([127, 0, 0, 1], 8000).into();
+    let server = Server::bind(&addr).serve(make_svc);
+
+    let temp_read = i2cfun(i2c_tx);
+
+    let (_, _) = tokio::join!(temp_read, server);
 }
