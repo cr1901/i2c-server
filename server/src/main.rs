57bc9d25eb0664a22e7d3d89b24d0f254f7622dc
William D. Jones <thor0505@comcast.net>
2020-07-19 07:34:02 -0400

    Thread arguments to rest of program.

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,11 +1,11 @@
 use std::convert::Infallible;
 
-#[cfg(windows)]
 use std::thread;
 use std::time::{Duration, SystemTime};
 use tokio::time::delay_for;
 use tokio::sync::Mutex;
 use std::sync::Arc;
+use std::path::Path;
 
 use clap::{Arg, ArgMatches, App, AppSettings, ArgGroup, SubCommand};
 use hyper::{Body, Method, Request, Response, StatusCode, Server};
@@ -19,8 +19,6 @@ use samples::SampleBuf;
 use i2cdev::core::*;
 #[cfg(unix)]
 use i2cdev::linux::{LinuxI2CDevice, LinuxI2CError};
-#[cfg(unix)]
-const TEMP_SENSOR_ADDR: u16 = 0x48;
 
 
 async fn temp_service(req: Request<Body>, rx: Arc<Mutex<SampleBuf<i16>>>) -> Result<Response<Body>, Infallible> {
@@ -40,8 +38,8 @@ async fn temp_service(req: Request<Body>, rx: Arc<Mutex<SampleBuf<i16>>>) -> Res
 
 // real code should probably not use unwrap()
 #[cfg(unix)]
-async fn i2cfun(tx: Arc<Mutex<SampleBuf<i16>>>) -> Result<(), ()> {
-    let mut dev = LinuxI2CDevice::new("/dev/i2c-1", TEMP_SENSOR_ADDR).unwrap();
+async fn measure<P>(path: P, addr: u16, tx: Arc<Mutex<SampleBuf<i16>>>) -> Result<(), ()> where P : AsRef<Path> {
+    let mut dev = LinuxI2CDevice::new(path, addr).unwrap();
 
     dev.smbus_write_byte_data(0x01, 0x60).unwrap();
 
@@ -58,15 +56,14 @@ async fn i2cfun(tx: Arc<Mutex<SampleBuf<i16>>>) -> Result<(), ()> {
     }
 }
 
-#[cfg(windows)]
-async fn i2cfun(tx: Arc<Mutex<SampleBuf<i16>>>) -> Result<(), ()> {
+async fn replay_synthesize(tx: Arc<Mutex<SampleBuf<i16>>>) -> Result<(), ()> {
     let mut fake_temp : i16 = -1024;
 
     loop {
         let now = SystemTime::now();
         thread::sleep(Duration::from_millis(1));
         let mut lock = tx.lock().await;
-        lock.post(now, fake_temp).expect("Fail!");
+        lock.post(now, fake_temp)?;
 
         if lock.len() == lock.capacity() {
             break;
@@ -90,6 +87,10 @@ fn parse_args<'a>() -> ArgMatches<'a> {
             .short("s")
             .value_name("RATE")
             .takes_value(true))
+        .arg(Arg::with_name("IP_ADDRESS")
+            .help("IP Address and Port")
+            .default_value("0.0.0.0:8000")
+            .index(1))
         .subcommand(SubCommand::with_name("measure")
             .about("Run the server and obtain data from I2C sensors (Unix only).")
             .arg(Arg::with_name("replay")
@@ -106,7 +107,7 @@ fn parse_args<'a>() -> ArgMatches<'a> {
                 .help("I2C device node")
                 .required(true)
                 .index(1))
-            .arg(Arg::with_name("ADDRESS")
+            .arg(Arg::with_name("I2C_ADDRESS")
                 .help("I2C device address")
                 .required(true)
                 .index(2)))
@@ -127,6 +128,7 @@ fn parse_args<'a>() -> ArgMatches<'a> {
 #[tokio::main]
 async fn main() {
     let matches = parse_args();
+
     let i2c_tx = Arc::new(Mutex::new(SampleBuf::<i16>::new(86400, 1)));
     let i2c_rx  = Arc::clone(&i2c_tx);
 
@@ -140,10 +142,25 @@ async fn main() {
        }
     });
 
-    let addr = ([0, 0, 0, 0], 8000).into();
+    let addr = matches.value_of("IP_ADDRESS").unwrap().parse().unwrap();
     let server = Server::bind(&addr).serve(make_svc);
 
-    let temp_read = i2cfun(i2c_tx);
+    if let Some(matches) = matches.subcommand_matches("measure") {
+        #[cfg(unix)]
+        {
+            let i2c_node = matches.value_of("NODE").unwrap();
+            let i2c_addr = u16::from_str_radix(matches.value_of("I2C_ADDRESS").unwrap(), 16).unwrap();
+            let (_, _) = tokio::join!(measure(i2c_node, i2c_addr, i2c_tx), server);
+        }
 
-    let (_, _) = tokio::join!(temp_read, server);
+        #[cfg(windows)]
+        println!("Measure subcommand only available on Unix systems.");
+    } else if let Some(matches) = matches.subcommand_matches("replay") {
+        if matches.is_present("synthesis") {
+            let replay_fn = replay_synthesize(i2c_tx);
+            let (_, _) = tokio::join!(replay_fn, server);
+        } else {
+            println!("Replay from file not yet implemented.");
+        }
+    }
 }
