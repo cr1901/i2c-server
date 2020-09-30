2e29547c03e9cb4755063d233e959d34729a52da
William D. Jones <thor0505@comcast.net>
2020-09-30 03:57:47 -0400

    tcn75a: Add unit tests for Limits.

--- tcn75a/src/limit.rs
+++ tcn75a/src/limit.rs
@@ -1,28 +1,26 @@
 use core::convert::{From, TryFrom};
 
+#[derive(Debug, PartialEq)]
 pub struct Limits(i16, i16);
-// pub struct LowerLimit(i16);
-// pub struct UpperLimit(i16);
 
 #[derive(Debug, PartialEq)]
 pub enum LimitError {
+    BothOutOfRange,
     LowOutOfRange,
     HighOutOfRange,
     LowExceedsHigh
 }
 
-// impl Limit {
-//     fn lower
-// }
-
 impl TryFrom<(i16, i16)> for Limits {
     type Error = LimitError;
 
     fn try_from(val: (i16, i16)) -> Result<Self, Self::Error> {
-        if val.0 < -256 || val.0 > 255 {
+        if (val.0 < -256 || val.0 > 255) && (val.1 < -256 || val.1 > 255) {
+            Err(LimitError::BothOutOfRange)
+        } else if val.0 < -256 || val.0 > 255 {
             Err(LimitError::LowOutOfRange)
         } else if val.1 < -256 || val.1 > 255 {
-            Err(LimitError::LowOutOfRange)
+            Err(LimitError::HighOutOfRange)
         } else if val.0 >= val.1 {
             Err(LimitError::LowExceedsHigh)
         } else {
@@ -34,5 +32,26 @@ impl TryFrom<(i16, i16)> for Limits {
 impl From<Limits> for (i16, i16) {
     fn from(limits: Limits) -> (i16, i16) {
         (limits.0, limits.1)
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_limit_ok() {
+        assert_eq!(TryFrom::try_from((0, 255)), Ok(Limits(0, 255)));
+        assert_eq!(TryFrom::try_from((-256, 255)), Ok(Limits(-256, 255)));
+        assert_eq!(TryFrom::try_from((-256, -1)), Ok(Limits(-256, -1)));
+    }
+
+    #[test]
+    fn test_limit_err() {
+        assert_eq!(<Limits as TryFrom<(i16, i16)>>::try_from((-257, 256)), Err(LimitError::BothOutOfRange));
+        assert_eq!(<Limits as TryFrom<(i16, i16)>>::try_from((0, 256)), Err(LimitError::HighOutOfRange));
+        assert_eq!(<Limits as TryFrom<(i16, i16)>>::try_from((-257, 0)), Err(LimitError::LowOutOfRange));
+        assert_eq!(<Limits as TryFrom<(i16, i16)>>::try_from((1, -1)), Err(LimitError::LowExceedsHigh));
     }
 }
