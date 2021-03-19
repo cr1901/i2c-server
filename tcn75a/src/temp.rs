ad8061a439f9c7e00cb5bc476b287b432a85197f
William D. Jones <thor0505@comcast.net>
2021-03-19 02:55:17 -0400

    tcn75a: Give Temperature struct documentation, remove Default trait (not needed anymore).

--- tcn75a/src/temp.rs
+++ tcn75a/src/temp.rs
@@ -2,10 +2,57 @@ use core::fmt;
 
 use fixed::types::I8F8;
 
-/* Invariant: temperature() will ensure lower bits are cleared for the
-given resolution. */
+/** A struct representing a temperature reading from the TCN75A.
 
-#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Default, Clone, Copy)]
+# Invariants
+
+[`Temperature`] is a [newtype] over the [`FixedI16::<U8>`] ([`I8F8`]) type provided by the
+[`fixed`] crate. The user cannot create this type; it mainly exists to provide a runtime
+guarantee that the contained data was successfully read from the TCN75A.
+
+# Examples
+
+To compare, add, subtract, compare, etc temperature measurements from a TCN75A, you should convert
+a [`Temperature`] to a [`I8F8`] type. [`Temperature`] implements [`Copy`], so a [`Temperature`]
+can be used simultaneously with its contained [`I8F8`].
+
+```
+# cfg_if::cfg_if! {
+# if #[cfg(any(target_os = "linux", target_os = "android"))] {
+# use linux_embedded_hal::I2cdev;
+# use embedded_hal::blocking::i2c::{Read, Write};
+# use tcn75a::{Tcn75a, Tcn75aError, ConfigReg, Resolution};
+# fn main() -> Result<(), Tcn75aError<<I2cdev as Read>::Error, <I2cdev as Write>::Error>> {
+# let i2c = I2cdev::new("/dev/i2c-1").unwrap();
+# let mut tcn = Tcn75a::new(i2c, 0x48);
+use fixed::types::I8F8;
+let temp0 : I8F8 = tcn.temperature()?.into();
+// ... Assume some time has passed.
+let temp1 : I8F8 = tcn.temperature()?.into();
+
+if temp0 < temp1 {
+    println!("Temperature is going up.");
+} else if temp0 > temp1 {
+    println!("Temperature is going down.");
+} else {
+    println!("Temperature is constant.");
+}
+# Ok(())
+# }
+# } else {
+# fn main() {
+# }
+# }
+# }
+```
+
+[`Temperature`]: ./struct.Temperature.html
+[newtype]: https://doc.rust-lang.org/rust-by-example/generics/new_types.html
+[`FixedI16::<U8>`]: ../fixed/struct.FixedI16.html
+[`I8F8`]: ../fixed/types/type.I8F8.html
+[`fixed`]: ../fixed/index.html
+[`Copy`]: https://doc.rust-lang.org/nightly/core/marker/trait.Copy.html */
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Clone, Copy)]
 pub struct Temperature(pub(crate) I8F8);
 
 impl From<Temperature> for I8F8 {
